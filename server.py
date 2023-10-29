5b760c8659b1a28f920f0a7d408ac6b5f469801b
vamsee <vamsee.somasi@gmail.com>
2023-10-29 19:44:28 +0530

    Forgot to add file to previous commit

--- server.py
+++ server.py
@@ -1,18 +1,68 @@
-from fastapi import FastAPI, UploadFile, Response
-from comparator import Comparator
+from fastapi import FastAPI, UploadFile, Form, Request
+from typing import Annotated
+from comparator import AssignmentEvaluator
+import sqlite3
+from pathlib import Path
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
+templates = Jinja2Templates(directory="templates")
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+conn = sqlite3.connect("assignment.db")
 
-@app.get("/")
-def start():
-    return {"Hello": "World"}
+# Create table to store file names and marks.
+create_assignment_table = """CREATE TABLE IF NOT EXISTS assignment(
+name VARCHAR(100) NOT NULL, 
+path VARCHAR(300) NOT NULL,
+marks VARCHAR(5)
+); 
+"""
+conn.cursor().execute(create_assignment_table)
+conn.close()
 
-@app.post("/pdfs")
-async def store_files(files: list[UploadFile], response: Response):
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# fetch assignments from db
+@app.get("/assignments")
+def fetch_assignments():
+    conn = sqlite3.connect("assignment.db")
+    result = conn.cursor().execute("SELECT * FROM assignment;").fetchall()
+    return result
+
+# Update marks of multiple assignments.
+@app.patch("/assignments/marks")
+def updateMarks(assignments: Annotated[list[dict], Form()]):
+    print(assignments[0].marks)
+
+@app.post("/upload")
+async def store_files(files: list[UploadFile]):
+    # Create input directory if not exists to store files
+    storage_dir = Path().cwd() / 'input'
+    storage_dir.mkdir(exist_ok=True)
+
+    # Create a db connection.
+    conn =  sqlite3.connect("assignment.db")
+
+    # Store files on local machine and add file names, paths in db
     for file in files:
+        name = file.filename
+        path = storage_dir / Path(f"{name}")
         content = file.file.read().decode("utf-8")
-        with open("./input/" + file.filename, "w") as outputFile:
+
+        # save file at path.
+        with open(path, "w") as outputFile:
             outputFile.write(content)
 
-    marks = Comparator().compute_marks_of_document(1)
-    return marks
+        # store metadata in db.
+        query = """
+INSERT INTO assignment(name, path) 
+VALUES(?, ?);
+"""
+        conn.cursor().execute(query, (name, str(path)))
+        conn.commit()
+    
+    conn.close()
