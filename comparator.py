c8c2f5e7ee6ae0531da76bf491ca78068084f8c2
vamsee <vamsee.somasi@gmail.com>
2023-10-29 19:42:12 +0530

    Refactor comparator

--- comparator.py
+++ comparator.py
@@ -3,29 +3,39 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 from utils import fetch_files, combine_similarities, compute_cosine_similarity
 
-class Comparator:
-    def __init__(self):
+class AssignmentEvaluator:
+    # data -> data is a list of files along with marks. {file, marks}
+    def __init__(self, data):
+        # files are loaded from /input directory.
         self.text_files = fetch_files()
-        self.marks = [8, 7, 10, None]
+        # data contains marks of each file.
+        self.assignments = data
 
     # compute_marks computes marks of a document (passed as index)
-    def compute_marks(self, documentIndex, similarities):
+    def compute_marks(self, documentIndex):
         numerator = 0
         denominator = 0
-        for index in range(len(similarities)):
+        for index in range(len(self.similarities)):
             # Skip documents with no marks.
             if (not self.marks[index]): continue
 
-            print(similarities[index][documentIndex])
-            numerator += similarities[index][documentIndex] * self.marks[index] 
-            denominator += similarities[index][documentIndex]
+            numerator += self.similarities[index][documentIndex] * self.marks[index] 
+            denominator += self.similarities[index][documentIndex]
         if denominator:
             marks = numerator/denominator
         else:
             marks = 0
         return marks
 
-    def compute_marks_of_document(self, documentIndex):
+    # get_similarity returns the similarity of two documents from similarity matrix.
+    def get_similarity(self, doc1, doc2, similarity_array):
+        doc1_index = self.text_files.index(doc1)                                                                                                                                                                                                                      
+        doc2_index = self.text_files.index(doc2)                                                                                                                                                                                                                      
+
+        return similarity_array[doc1_index][doc2_index]
+
+    # fit function creates a similarity matrix between all uploaded assignments.
+    def fit(self):
         documents = [open("./input/" + f).read() for f in self.text_files]
         tfidf = TfidfVectorizer().fit_transform(documents)
         countVectors = CountVectorizer().fit_transform(documents).toarray()
@@ -36,16 +46,9 @@ class Comparator:
         count_similarity = compute_cosine_similarity(countVectors)
 
         # Combine count vector and tfidf scores.
-        similarity = combine_similarities(count_similarity, 0.3, tf_similarity, 0.7)
+        self.similarities = combine_similarities(count_similarity, 0.3, tf_similarity, 0.7)
 
-        return self.compute_marks(documentIndex, similarity)
-
-    # get_similarity returns the similarity of two documents from similarity matrix.
-    def get_similarity(self, doc1, doc2, similarity_array):
-        doc1_index = self.text_files.index(doc1)                                                                                                                                                                                                                      
-        doc2_index = self.text_files.index(doc2)                                                                                                                                                                                                                      
-
-        return similarity_array[doc1_index][doc2_index]
+        return self.compute_marks(1)
 
 
 
